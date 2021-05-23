ba9e55fdc8d9b4d50ea59b9fbaf35ae0c4a0d997
tlpackus <tlpackus@gmail.com>
2021-05-23 03:14:16 -0700

    add editing functionality for stylists details

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -9,12 +9,10 @@ namespace HairSalon.Controllers
   public class StylistsController : Controller
   {
     private readonly HairSalonContext _db;
-
     public StylistsController(HairSalonContext db)
     {
       _db = db;
     }
-
     public ActionResult Index()
     {
       List<Stylist> model = _db.Stylists.ToList();
@@ -36,6 +34,18 @@ namespace HairSalon.Controllers
     {
       Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId ==id);
       return View(thisStylist);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 } 
