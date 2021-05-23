a529d90e67e3eba9d0cefa24ad1ac122f22fe30a
tlpackus <tlpackus@gmail.com>
2021-05-23 02:50:05 -0700

    add functionality to add new stylists and link them to clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -18,13 +18,12 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      // List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       return View(model);
     }
     public ActionResult Create()
     {
-      // ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name";)
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
@@ -43,7 +42,7 @@ namespace HairSalon.Controllers
     public ActionResult Edit(int id)
     {
       var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-      // ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(thisClient);
     }
 
